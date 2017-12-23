477dddd5415dcc258d7a63a39f6f2ea1ec30c400
hhornbacher <harry@harry-hornbacher.de>
2017-12-23 01:17:37 +0100

    improved ResponsePacket

--- src/ResponsePacket.cxx
+++ src/ResponsePacket.cxx
@@ -1,1 +1,20 @@
 #include "ResponsePacket.h"
+
+ResponsePacket::ResponsePacket(uint8_t *data)
+{
+    memcpy(&rawPacket, data, IDSO10790A_PACKET_SIZE);
+}
+
+uint8_t *ResponsePacket::getHeader()
+{
+    return rawPacket.header;
+}
+uint8_t *ResponsePacket::getPayload()
+{
+    return rawPacket.payload;
+}
+size_t ResponsePacket::getPayloadLength()
+{
+    uint8_t *ptrOffset = (uint8_t *)memchr(rawPacket.payload, 0x5a, IDSO10790A_PACKET_SIZE);
+    return (size_t)(rawPacket.payload - ptrOffset);
+}
--- src/ResponsePacket.h
+++ src/ResponsePacket.h
@@ -2,17 +2,29 @@
 #define _RESPONSE_PACKET_H_
 
 #include <cstdint>
+#include <cstring>
 
 #include <deque>
 
 #define IDSO10790A_PACKET_SIZE 509
 
-struct __attribute__((packed)) Packet
+class ResponsePacket
 {
-    uint8_t header[7];
-    uint8_t payload[IDSO10790A_PACKET_SIZE - 7];
+  private:
+    struct __attribute__((packed)) Packet
+    {
+        uint8_t header[7];
+        uint8_t payload[IDSO10790A_PACKET_SIZE - 7];
+    } rawPacket;
+
+  public:
+    ResponsePacket(uint8_t *data);
+
+    uint8_t *getHeader();
+    uint8_t *getPayload();
+    size_t getPayloadLength();
 };
 
-typedef std::deque<Packet *> PacketQueue;
+typedef std::deque<ResponsePacket *> PacketQueue;
 
 #endif // _RESPONSE_PACKET_H_
