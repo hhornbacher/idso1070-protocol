94d1576d1f91a7d606df2d680b45b2151bfedb27
hhornbacher <harry@harry-hornbacher.de>
2017-12-26 23:13:06 +0100

    moved CommandCode to enums.h

--- lib/enums.h
+++ lib/enums.h
@@ -1,6 +1,32 @@
 #ifndef _ENUMS_H_
 #define _ENUMS_H_
 
+enum CommandCode
+{
+    CMD_FORCE_TRIGGER = 0x01,
+    CMD_TRIGGER_MODE = 0x02,
+    CMD_SET_RELAY = 0x03,
+    CMD_CHANNEL_SELECTION = 0x04,
+    CMD_TRIGGER_SOURCE = 0x05,
+    CMD_CHANNEL_VOLTS_DIV_125 = 0x06,
+    CMD_PRE_TRIGGER_LENGTH = 0x07,
+    CMD_POST_TRIGGER_LENGTH = 0x08,
+    CMD_RAM_START_POSITION = 0x09,
+    CMD_RESERVED_DATA_OUTPUT = 0x0a,
+    CMD_CH1_PWM = 0x0b,
+    CMD_CH2_PWM = 0x0c,
+    CMD_TRIGGER_PWM = 0x0d,
+    CMD_LOGIC_ANALYZER_1 = 0x0e,
+    CMD_LOGIC_ANALYZER_2 = 0x0f,
+    CMD_LOGIC_ANALYZER_4 = 0x10,
+    CMD_SAMPLE_RATE = 0x11,
+    CMD_FREQ_DIV_LOW = 0x12,
+    CMD_FREQ_DIV_HIGH = 0x13,
+    CMD_SERIAL_BAUD_RATE = 0x14,
+    CMD_RAM_CHANNEL_SELECTION = 0x15,
+    CMD_READ_RAM_COUNT = 0x16
+};
+
 enum TimeBase
 {
     HDIV_5nS = 0,
@@ -70,6 +96,28 @@ enum ScopeMode
 {
     SCOMODE_ANALOG,
     SCOMODE_DIGITAL
+};
+
+enum TriggerChannel
+{
+    TRIGCHAN_CH1,
+    TRIGCHAN_CH2,
+    TRIGCHAN_EXT,
+    TRIGCHAN_EXT10,
+    TRIGCHAN_ALT
+};
+
+enum TriggerMode
+{
+    TRIGMODE_AUTO,
+    TRIGMODE_NORMAL,
+    TRIGMODE_SINGLE
+};
+
+enum TriggerSlope
+{
+    TRIGSLOPE_RISING,
+    TRIGSLOPE_FALLING
 };
 
 #endif // _ENUMS_H_
