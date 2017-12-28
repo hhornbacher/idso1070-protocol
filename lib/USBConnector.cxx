39aad28b8987944c863e511da167c9e11e46a8d5
hhornbacher <harry@harry-hornbacher.de>
2017-12-29 00:18:58 +0100

    implemented USBConnector

--- lib/USBConnector.cxx
+++ lib/USBConnector.cxx
@@ -1,1 +1,74 @@
 #include "USBConnector.h"
+
+USBConnector::USBConnector(const char *device)
+{
+    strncpy(this->device, device, 256);
+}
+
+USBConnector::~USBConnector()
+{
+    if (handle < 0)
+        close(handle);
+}
+
+void USBConnector::start()
+{
+    termios tty;
+    handle = open(device, O_RDWR | O_NOCTTY | O_SYNC);
+    memset(&tty, 0, sizeof tty);
+    if (tcgetattr(handle, &tty) != 0)
+    {
+        printf("error %d from tcgetattr", errno);
+    }
+
+    cfsetospeed(&tty, USB_SERIAL_SPEED);
+    cfsetispeed(&tty, USB_SERIAL_SPEED);
+
+    tty.c_cflag = (tty.c_cflag & ~CSIZE) | CS8; // 8-bit chars
+    // disable IGNBRK for mismatched speed tests; otherwise receive break
+    // as \000 chars
+    tty.c_iflag &= ~IGNBRK; // disable break processing
+    tty.c_lflag = 0;        // no signaling chars, no echo,
+                            // no canonical processing
+    tty.c_oflag = 0;        // no remapping, no delays
+    tty.c_cc[VMIN] = 0;     // read doesn't block
+    tty.c_cc[VTIME] = 5;    // 0.5 seconds read timeout
+
+    tty.c_iflag &= ~(IXON | IXOFF | IXANY); // shut off xon/xoff ctrl
+
+    tty.c_cflag |= (CLOCAL | CREAD);   // ignore modem controls,
+                                       // enable reading
+    tty.c_cflag &= ~(PARENB | PARODD); // shut off parity
+    tty.c_cflag &= ~CSTOPB;
+    tty.c_cflag &= ~CRTSCTS;
+
+    tty.c_cc[VMIN] = 0;  // Set non-blocking
+    tty.c_cc[VTIME] = 5; // 0.5 seconds read timeout
+
+    if (tcsetattr(handle, TCSANOW, &tty) != 0)
+    {
+        printf("error %d from tcsetattr", errno);
+    }
+}
+
+void USBConnector::stop()
+{
+    close(handle);
+    handle = -1;
+}
+
+void USBConnector::transmit(uint8_t *data, size_t length)
+{
+    write(handle, data, length);
+}
+
+size_t USBConnector::receive()
+{
+    if (packetBufferLength < PACKETBUFFER_LENGTH)
+    {
+        int result = read(handle, &packetBuffer[packetBufferLength], PACKETBUFFER_LENGTH - packetBufferLength);
+        if (result > 0)
+            packetBufferLength += result;
+    }
+    return packetBufferLength;
+}
--- lib/USBConnector.h
+++ lib/USBConnector.h
@@ -1,23 +1,27 @@
 #ifndef _USB_CONNECTOR_H_
 #define _USB_CONNECTOR_H_
 
+#include <termios.h>
+
 #include "Connector.h"
+
+#define USB_SERIAL_SPEED B4000000
 
 class USBConnector : public Connector
 {
-  private:
-    int socketHandle;
-    sockaddr_in serverAddress;
+private:
+  char device[256];
+  int handle;
 
-  public:
-    USBConnector(char *device);
-    ~USBConnector();
+public:
+  USBConnector(const char *device);
+  ~USBConnector();
 
-    void transmit(uint8_t *data, size_t length);
-    size_t receive();
+  void transmit(uint8_t *data, size_t length);
+  size_t receive();
 
-    void start();
-    void stop();
+  void start();
+  void stop();
 };
 
 #endif // _USB_CONNECTOR_H_
